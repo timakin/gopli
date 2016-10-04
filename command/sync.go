c731b6c97cead9aa3df77c1fd2c903dd7a5170c5
timakin <timaki.st@gmail.com>
2016-10-05 01:47:10 +0900

    [add] toml config loader

--- command/sync.go
+++ command/sync.go
@@ -1,8 +1,35 @@
 package command
 
-import "github.com/codegangsta/cli"
+import (
+	"github.com/codegangsta/cli"
+	"github.com/BurntSushi/toml"
+	"github.com/k0kubun/pp"
+)
+
+type tomlConfig struct {
+    Database map[string]database
+    SSH map[string]ssh
+}
+
+type database struct {
+    Host string
+    Port int
+    ManagementSystem string
+		Name string
+		User string
+    Password string
+}
+
+type ssh struct {
+    Key string
+}
 
 func CmdSync(c *cli.Context) {
-	// Write your code here
-
+	pp.Print(c.String("config"))
+	var tmlconf tomlConfig
+	if _, err := toml.DecodeFile(c.String("config"), &tmlconf); err != nil {
+		// TODO: pkg/errors
+		pp.Print(err)
+	}
+	pp.Print(tmlconf)
 }
